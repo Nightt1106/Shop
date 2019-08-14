67f43b3298b212b63e4775c1857baf11a61ad695
night <40443125@gm.nfu.edu.tw>
2019-08-14 21:14:57 +0800

    Retrofit!!!!!!!!!!!!!!!!! and Homework Done

--- app/src/main/java/com/Night/shop/BusActivity.kt
+++ app/src/main/java/com/Night/shop/BusActivity.kt
@@ -1,12 +1,20 @@
 package com.Night.shop
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 import kotlinx.android.synthetic.main.activity_bus.*
+import kotlinx.android.synthetic.main.content_bus.*
+import kotlinx.android.synthetic.main.row_bus_function.view.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.info
@@ -20,7 +28,7 @@ import retrofit2.http.GET
 
 class BusActivity : AppCompatActivity(),AnkoLogger {
 
-    var busses : Busses? = null
+    var bus : Busses? = null
     val retrofit = Retrofit.Builder()
         .baseUrl("https://data.tycg.gov.tw/opendata/datalist/datasetMeta/")
         .addConverterFactory(GsonConverterFactory.create())
@@ -31,32 +39,58 @@ class BusActivity : AppCompatActivity(),AnkoLogger {
         setContentView(R.layout.activity_bus)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        /*fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
 
         //TODO: BUS
 
         doAsync {
 
-            val bussesSevice = retrofit.create(BussesSevice::class.java)
-            busses = bussesSevice.listBus()
+            val bussesSevice = retrofit.create(BusSevice::class.java)
+            bus = bussesSevice.listBus()
                 .execute()
                 .body()
 
-            busses?.datas?.forEach {
+            bus?.datas?.forEach {
                 info {
                     it.BusID
-                    it.BusStatus
-                    it.DataTime
                 }
             }
             
             uiThread {
-
+                recycler_bus.layoutManager = LinearLayoutManager(this@BusActivity)
+                recycler_bus.setHasFixedSize(true)
+                recycler_bus.addItemDecoration(DividerItemDecoration(this@BusActivity,DividerItemDecoration.VERTICAL))
+                recycler_bus.adapter = BusAdapter()
             }
 
+        }
+    }
+
+    inner class BusAdapter() : RecyclerView.Adapter<BusHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BusHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.row_bus_function,parent,false)
+            return  BusHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return bus?.datas?.size ?:0
+        }
+
+        override fun onBindViewHolder(holder: BusHolder, position: Int) {
+            bus!!.datas[position].let {
+                holder.bindBus(it)
+            }
+        }
+    }
+
+    inner class BusHolder(view: View) : RecyclerView.ViewHolder(view){
+        val busId = view.bus_id
+        fun bindBus(bus:Data){
+            busId.text = bus.BusID
         }
     }
 }
@@ -82,7 +116,7 @@ data class Data(
     val sections: String
 )
 
-interface BussesSevice {
+interface BusSevice {
     @GET("download?id=b3abedf0-aeae-4523-a804-6e807cbad589&rid=bf55b21a-2b7c-4ede-8048-f75420344aed")
     fun listBus(): Call<Busses>
 }
